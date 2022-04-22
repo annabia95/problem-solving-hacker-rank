1d8948e6f4efca9f6dd82d64e3489a47495dcc8f
Anna Beatriz Garcia <annagarcia@id.uff.br>
2022-04-22 15:55:28 -0300

    Adiciona exercicio 1

---
+++ 01-operacoesAritmeticas.js
@@ -0,0 +1,15 @@
+/* EXERCICIO 1
+Faça as 4 operações básicas com base nos números 2 e 4
+Ao final de cada operação imprima na tela no seguinte formato:
+Soma : 2 + 4 = x
+Subtração: 2 - 4 = x
+Divisão: 2 / 4 = x
+Multiplicação: 2 * 4 = x */
+
+const num1 = 2;
+const num2 = 4;
+
+console.log('Soma : 2 + 4 = ' + (num1 + num2));
+console.log('Subtração: 2 - 4 = ' + (num1 - num2));
+console.log('Divisão: 2 / 4 = ' + (num1 / num2));
+console.log('Multiplicação: 2 * 4 = ' + (num1 * num2));
