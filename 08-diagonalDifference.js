2501a116a541d01407d8ce842be8df9cb8b71cd7
Anna Beatriz Garcia <annagarcia@id.uff.br>
2022-04-24 17:04:30 -0300

    Adiciona exercicio 8

---
+++ 08-diagonalDifference.js
@@ -0,0 +1,14 @@
+function diagonalDifference(arr) {
+  let leftDiagonal = 0;
+  let rightDiagonal = 0;
+    for(let index = 0; index < arr.length; index +=1) {
+      leftDiagonal = leftDiagonal + arr[index][index];
+      rightDiagonal = rightDiagonal + arr[index][arr.length -1- index];
+    }
+ return Math.abs(leftDiagonal - rightDiagonal);
+}
+
+console.log (diagonalDifference([
+  [1,2,3],
+  [4,5,6],
+  [7,8,9]]));
